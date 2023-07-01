6db9b00b8dc369d7975b81b10e1599b639258661
Artem Matevosian <a.matevosian@innopolis.university>
2023-07-01 23:24:24 +0000

    B21 to B21 (russian to latin letter)

--- src/output_formatting/output_algorithms.py
+++ src/output_formatting/output_algorithms.py
@@ -3,7 +3,7 @@ from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
 
 
 WEEKDAYS = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
-YEARS = ('B22', 'В21', 'B20', 'B19')
+YEARS = ('B22', 'B21', 'B20', 'B19')
 PERIODS = ("9:00-10:30", "10:40-12:10", "12:40-14:10", "14:20-15:50", "16:00-17:30", "17:40-19:00")
 
 
