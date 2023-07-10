7b5909552f7c5e7db7303c9c948d8dcac309eaac
ArtMGreen <artyom5145@gmail.com>
2023-07-10 14:39:48 +0300

    Total block tables are introduced

--- src/output_formatting/output_algorithms.py
+++ src/output_formatting/output_algorithms.py
@@ -1,5 +1,6 @@
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
+from openpyxl.utils import get_column_letter
 
 WEEKDAYS = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
 YEARS = ('B22', 'B21', 'B20', 'B19')
@@ -26,6 +27,11 @@ def parametrized(week):
 # week format: (weekday, class_number, group_name) -> (room_number, class_name, instructor)
 def create_xlsx(block1, block2, groups):
     wb = Workbook()
+    total_block1 = wb.active
+    total_block1.title = "Block 1"
+    total_block2 = wb.create_sheet("Block 2")
+    rest_width_offset = 0
+
     for year in YEARS:
         year_groups = list(filter(lambda x: x.startswith(year), groups))
         entries_block1 = list(filter(lambda x: x[2] in year_groups, block1.keys()))
@@ -33,44 +39,69 @@ def create_xlsx(block1, block2, groups):
         year_sheet_block1 = wb.create_sheet(year + " (block 1)")
         year_sheet_block2 = wb.create_sheet(year + " (block 2)")
 
+        # denoting groups and weekdays
         for column_number, group in enumerate(sorted(year_groups), 2):
             year_sheet_block1.cell(row=1, column=column_number, value=group)
             year_sheet_block2.cell(row=1, column=column_number, value=group)
+            total_block1.cell(row=1, column=column_number + rest_width_offset, value=group)
+            total_block2.cell(row=1, column=column_number + rest_width_offset, value=group)
         for weekday in WEEKDAYS:
             year_sheet_block1.cell(row=get_row_offset(weekday, 0), column=1, value=weekday)
             year_sheet_block2.cell(row=get_row_offset(weekday, 0), column=1, value=weekday)
+            total_block1.cell(row=get_row_offset(weekday, 0), column=1, value=weekday)
+            total_block2.cell(row=get_row_offset(weekday, 0), column=1, value=weekday)
             for class_number in range(0, 6):
                 year_sheet_block1.cell(row=get_row_offset(weekday, class_number) + 1, column=1,
                                        value=PERIODS[class_number])
                 year_sheet_block2.cell(row=get_row_offset(weekday, class_number) + 1, column=1,
                                        value=PERIODS[class_number])
+                total_block1.cell(row=get_row_offset(weekday, class_number) + 1, column=1,
+                                  value=PERIODS[class_number])
+                total_block2.cell(row=get_row_offset(weekday, class_number) + 1, column=1,
+                                  value=PERIODS[class_number])
 
         for slot in entries_block1:
             weekday = slot[0]
             class_number = slot[1]
             column_number = sorted(year_groups).index(slot[2]) + 2
             row_number = get_row_offset(weekday, class_number)
+
             room_number = block1[slot][0]
             year_sheet_block1.cell(row=row_number + 3, column=column_number, value=room_number)
+            total_block1.cell(row=row_number + 3, column=column_number+rest_width_offset, value=room_number)
+
             class_name = block1[slot][1]
             year_sheet_block1.cell(row=row_number + 1, column=column_number, value=class_name)
+            total_block1.cell(row=row_number + 1, column=column_number+rest_width_offset, value=class_name)
+
             instructor = block1[slot][2]
             year_sheet_block1.cell(row=row_number + 2, column=column_number, value=instructor)
+            total_block1.cell(row=row_number + 2, column=column_number+rest_width_offset, value=instructor)
 
         for slot in entries_block2:
             weekday = slot[0]
             class_number = slot[1]
             column_number = sorted(year_groups).index(slot[2]) + 2
             row_number = get_row_offset(weekday, class_number)
+
             room_number = block2[slot][0]
             year_sheet_block2.cell(row=row_number + 3, column=column_number, value=room_number)
+            total_block2.cell(row=row_number + 3, column=column_number+rest_width_offset, value=room_number)
+
             class_name = block2[slot][1]
             year_sheet_block2.cell(row=row_number + 1, column=column_number, value=class_name)
+            total_block2.cell(row=row_number + 1, column=column_number+rest_width_offset, value=class_name)
+
             instructor = block2[slot][2]
             year_sheet_block2.cell(row=row_number + 2, column=column_number, value=instructor)
+            total_block2.cell(row=row_number + 2, column=column_number+rest_width_offset, value=instructor)
 
+        rest_width_offset += len(year_groups)
         prettify(year_sheet_block1, len(year_groups))
         prettify(year_sheet_block2, len(year_groups))
+
+    prettify(total_block1, rest_width_offset)
+    prettify(total_block2, rest_width_offset)
     wb.save("schedule.xlsx")
 
 
@@ -82,7 +113,8 @@ def get_row_offset(weekday, class_number):
 def prettify(sheet, number_of_groups):
     # formatting the whole table
     alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
-    for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:1 + number_of_groups]:
+    for column in range(1+number_of_groups):
+        letter = get_column_letter(column+1)
         sheet.column_dimensions[letter].auto_size = True
         sheet.column_dimensions[letter].width += 1
         for row in range(1, get_row_offset("Sun", 6) + 4):
@@ -95,7 +127,8 @@ def prettify(sheet, number_of_groups):
                     top=Side(border_style="thin", color='FF000000'),
                     bottom=Side(border_style="thin", color='FF000000'))
     fill = PatternFill(fill_type="darkGrid", start_color="00AA00", end_color="00AA00")
-    for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:1 + number_of_groups]:
+    for column in range(1 + number_of_groups):
+        letter = get_column_letter(column + 1)
         sheet[letter + "1"].font = font
         sheet[letter + "1"].fill = fill
         sheet[letter + "1"].border = border
@@ -107,13 +140,14 @@ def prettify(sheet, number_of_groups):
     fill = PatternFill(fill_type="solid", start_color="22FF22", end_color="22FF22")
     for weekday in WEEKDAYS:
         row = str(get_row_offset(weekday, 0))
-        for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:1 + number_of_groups]:
+        for column in range(1 + number_of_groups):
+            letter = get_column_letter(column + 1)
             sheet[letter + row].font = font
             sheet[letter + row].fill = fill
             sheet[letter + row].border = border
 
     # formatting class periods
-    sheet.column_dimensions["A"].width = 10
+    sheet.column_dimensions["A"].width = 12
     border = Border(left=Side(border_style="thin", color='FF000000'),
                     right=Side(border_style="thin", color='FF000000'),
                     top=Side(border_style="thin", color='FF000000'),
@@ -130,8 +164,8 @@ def prettify(sheet, number_of_groups):
             for column in range(2, 2 + number_of_groups):
                 sheet.cell(column=column, row=row_offset + 1).border = \
                     Border(left=Side(border_style="thin", color='FF000000'),
-                          right=Side(border_style="thin", color='FF000000'),
-                          top=Side(border_style="thin", color='FF000000'))
+                           right=Side(border_style="thin", color='FF000000'),
+                           top=Side(border_style="thin", color='FF000000'))
                 sheet.cell(column=column, row=row_offset + 2).border = \
                     Border(left=Side(border_style="thin", color='FF000000'),
                            right=Side(border_style="thin", color='FF000000'))
@@ -161,7 +195,7 @@ def prettify(sheet, number_of_groups):
                 start_column = column
             else:
                 if start_column != column - 1:
-                    sheet.merge_cells(start_column=start_column, end_column=column-1,
+                    sheet.merge_cells(start_column=start_column, end_column=column - 1,
                                       start_row=row_number, end_row=row_number)
                 start_column = column
                 current_value = sheet.cell(row=row_number, column=column).value
