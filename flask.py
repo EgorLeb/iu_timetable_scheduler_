a4175168b544951610037e62a040c6bcedf511d5
Sofia Gamershmidt <s.gamershmidt@innopolis.university>
2023-07-15 17:29:45 +0000

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,10 +1,10 @@
 import os, time
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-from src.main.script import get_schedule
+from script2 import get_schedule
 from src.output_formatting.output_algorithms import parametrized, create_xlsx
 
-UPLOAD_FOLDER = 'input_data'
+UPLOAD_FOLDER = 'mysite/input_data'
 DOWNLOAD_FOLDER = 'output_data'
 ALLOWED_EXTENSIONS = set(['xlsx'])
 
@@ -23,9 +23,8 @@ def upload():
         print(file.filename)
         if file and allowed_file(file.filename):
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], "Time_Table_Input.xlsx"))
-            week, groups = get_schedule()
-            
-            create_xlsx(parametrized(week), groups)
+            week1, week2, groups = get_schedule()
+            create_xlsx(parametrized(week1), parametrized(week2), groups)
             return redirect(url_for('download'))
     return  render_template('index.html')
 @app.route('/download/', methods=['GET', 'POST'])
@@ -33,12 +32,12 @@ def download():
     if request.method == 'POST':
         print(request)
         return send_from_directory(os.path.join(app.config['DOWNLOAD_FOLDER']), "schedule.xlsx")
-    return render_template('download.html') 
+    return render_template('download.html')
 
 @app.route('/schedule', methods=['GET', 'POST'])
 def schedule():
-   print(1) 
-   return render_template('download.html') 
+   print(1)
+   return render_template('download.html')
 
-if(__name__ == "__main__"):
-    app.run(debug=True)
+# if(__name__ == "__main__"):
+#     app.run(debug=True)
