ec4c5a0b28e624676b9b19ee7a9bf33f25574e98
Sofia Gamershmidt <s.gamershmidt@innopolis.university>
2023-07-16 11:27:13 +0000

    Update flask

--- flask.py
+++ flask.py
@@ -1,8 +1,16 @@
 import os, time
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-from script2 import get_schedule
+from script import get_schedule
 from src.output_formatting.output_algorithms import parametrized, create_xlsx
+from src.class_hierarchy.exception_classes.always_no_as_preference import AlwaysNoAsTeacherPreference
+from src.class_hierarchy.exception_classes.no_such_group_exists import NoSuchGroupExists
+from src.class_hierarchy.exception_classes.no_teacher_preference import NoTeacherPreferenceException
+from src.class_hierarchy.exception_classes.not_a_number_provided import NotANumberProvided
+from src.class_hierarchy.exception_classes.wrong_course_activity_format import WrongCourseActivityFormat
+from src.class_hierarchy.exception_classes.wrong_course_activity_type import WrongCourseActivityTypeException
+from src.class_hierarchy.exception_classes.wrong_format_for_sport_reservation import WrongFormatForSportReservation
+from src.class_hierarchy.exception_classes.wrong_sheet_name import WrongSheetNameProvided
 
 UPLOAD_FOLDER = 'mysite/input_data'
 DOWNLOAD_FOLDER = 'output_data'
@@ -23,10 +31,31 @@ def upload():
         print(file.filename)
         if file and allowed_file(file.filename):
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], "Time_Table_Input.xlsx"))
-            week1, week2, groups = get_schedule()
-            create_xlsx(parametrized(week1), parametrized(week2), groups)
-            return redirect(url_for('download'))
-    return  render_template('index.html')
+            try:
+
+                week1, week2, groups, rooms = get_schedule()
+                create_xlsx(week1, week2, groups, rooms)
+
+                return redirect(url_for('download'))
+            except AlwaysNoAsTeacherPreference as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except NoSuchGroupExists as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except NoTeacherPreferenceException as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except NotANumberProvided as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except WrongCourseActivityFormat as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except WrongCourseActivityTypeException as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except WrongFormatForSportReservation as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except WrongSheetNameProvided as e:
+                return render_template('index.html', upload = False, message = e.args[0])
+            except AttributeError:
+                return render_template('index.html', upload = False, message = "Empty cell error")
+    return render_template('index.html', upload = True, message = "Input file verification")
 @app.route('/download/', methods=['GET', 'POST'])
 def download():
     if request.method == 'POST':
